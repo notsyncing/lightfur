edd5f42c7edcfcc0bd39b8bfe3784927daeb26bd
0xfc <song.fc@gmail.com>
2016-10-09 11:06:30 +0800

    entity: 去除不必要的代码。

--- lightfur-entity/src/main/kotlin/io/github/notsyncing/lightfur/entity/utils/EntityFieldUtils.kt
+++ lightfur-entity/src/main/kotlin/io/github/notsyncing/lightfur/entity/utils/EntityFieldUtils.kt
@@ -1,12 +1,5 @@
 package io.github.notsyncing.lightfur.entity.utils
 
-import io.github.notsyncing.lightfur.entity.EntityFieldInfo
-import io.github.notsyncing.lightfur.entity.annotations.EntityColumn
-import kotlin.reflect.KProperty
-
 object EntityFieldUtils {
-    fun getFieldInfo(property: KProperty<*>): EntityFieldInfo {
-        val a = property.javaClass.getAnnotation(EntityColumn::class.java)
-        return EntityFieldInfo(property.name, a.name, a.type, a.length, a.nullable, a.defaultValue, a.primaryKey)
-    }
+
 }
